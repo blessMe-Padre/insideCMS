05ae99df11ff2ffe3c1f5ab770085df3b4038f85
Сергей Субботин <zarodiny@yandex.ru>
2025-10-16 11:13:41 +1000

    feat: обновляет компонент PageBuilderForm для добавления и удаления элементов с использованием попапа и селекта

--- insideCMS/resources/js/components/form/PageBuilderForm.tsx
+++ insideCMS/resources/js/components/form/PageBuilderForm.tsx
@@ -1,27 +1,41 @@
 import { useState } from 'react';
 
 import { Button } from '@/components/ui/button';
-import { PlusIcon } from 'lucide-react';
+import { PlusIcon, TrashIcon } from 'lucide-react';
+import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
 
-const elementsList = [
-    { name: 'text-block', type: 'text' },
-    { name: 'image-block', type: 'file' },
-    { name: 'text-editor-block', type: 'text-editor'},
-]
+// const elementsList = [
+//     { name: 'text-block', type: 'text', description: 'Текстовый блок' },
+//     { name: 'image-block', type: 'file', description: 'Изображение' },
+//     { name: 'text-editor-block', type: 'text-editor', description: 'Текстовый редактор' },
+// ]
 
 export default function PageBuilderForm() {
     const [elements, setElements] = useState<Array<{ id: string; type: string }>>([]);
-
-    console.log(elementsList);
-
+    const [selectedElement, setSelectedElement] = useState<string>('');
+    const [isPopoverOpen, setIsPopoverOpen] = useState<boolean>(false);
 
     const handleAddElement = () => {
+        if (!selectedElement) return;
+        
         const newElement = {
-            id: `element-${Date.now()}`,
-            type: 'text-block'
+          id: `element-${Date.now()}`,
+          type: selectedElement
         };
+        
         setElements([...elements, newElement]);
+        setSelectedElement('');
+        setIsPopoverOpen(false);
+      }
+
+    const handleRemoveElement = (id: string) => {
+        setElements(elements.filter((element) => element.id !== id));
+    }
+
+    const handleSelectElement = (value: string) => {
+        setSelectedElement(value);
     }
 
 
@@ -30,21 +44,42 @@ export default function PageBuilderForm() {
             <h1 className="font-bold mb-4">Создайте страницу</h1>
 
             {elements.map((element) => (
-                <div key={element.id} className="mb-4 p-4 border rounded">
+                <div key={element.id} className="mb-4 p-4 border rounded flex items-center justify-between">
                     <h2>{element.type}</h2>
+                    <button className="cursor-pointer" onClick={() => handleRemoveElement(element.id)}><TrashIcon/></button>
                 </div>
             ))}
+         
 
-            <Button 
-                variant="outline" 
-                className="flex items-center justify-center w-full h-full gap-2 cursor-pointer transition-all"
-                onClick={handleAddElement}
-                >
-                <PlusIcon className="size-4 text-gray-500"/>
-                <span>Добавить элемент</span>
-            </Button>
+            <Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
+                <PopoverTrigger className="flex items-center justify-center w-full h-full gap-2 cursor-pointer transition-all">
+                    <PlusIcon className="size-4 text-gray-500"/>
+                    <span>Добавить элемент</span>
+                </PopoverTrigger>
 
-            {/* Сделать вместо кнопки селект с элементами */}
+                <PopoverContent>
+                    <Select onValueChange={handleSelectElement}>
+                        <SelectTrigger className="w-[180px]">
+                            <SelectValue placeholder="Выберите элемент" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="text-block">Текстовый блок</SelectItem>
+                            <SelectItem value="image-block">Изображение</SelectItem>
+                            <SelectItem value="text-editor-block">Текстовый редактор</SelectItem>
+                        </SelectContent>
+                    </Select>
+                    <Button 
+                        variant="outline" 
+                        className="flex items-center justify-center w-full h-full gap-2 cursor-pointer transition-all"
+                        onClick={handleAddElement}
+                        disabled={!selectedElement}
+                        >
+                        <PlusIcon className="size-4 text-gray-500"/>
+                        <span>Добавить элемент</span>
+                    </Button>
+                </PopoverContent>
+            </Popover>
+
         </div>
     );
 }
