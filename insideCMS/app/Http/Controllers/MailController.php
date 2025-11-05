85f1d55d11e0118a07b6373ec34c5e141725a424
Сергей Субботин <zarodiny@yandex.ru>
2025-11-06 09:27:51 +1000

    feat: обновляет MailController для отправки писем на несколько адресов из настроек сайта

--- insideCMS/app/Http/Controllers/MailController.php
+++ insideCMS/app/Http/Controllers/MailController.php
@@ -8,8 +8,9 @@ use Inertia\Inertia;
 use Inertia\Response;
 use App\Mail\MailForm;
 use Illuminate\Support\Facades\Mail;
+use App\Models\Site_settings;
 
-
+    
 class MailController extends Controller
 {
 
@@ -21,7 +22,14 @@ class MailController extends Controller
     public function send_email(MailFormRequest $request)
     {
         try {
-            Mail::to('inside-dev360@yandex.ru')->send(new MailForm($request->validated()));
+            $settings = Site_settings::where('slug', 'emails')->first();
+            $emails = $settings->content;
+
+            // Перебираем массив emails и отправляем письмо на каждый email
+            foreach ($emails as $email) {
+                Mail::to($email)->send(new MailForm($request->validated()));
+            }
+            
             return redirect()->route('mail')->with('success', 'Письмо успешно отправлено на email');
         } catch (\Exception $e) {
             return redirect()->route('mail')->with('error', 'Ошибка отправки письма: ' . $e->getMessage());
