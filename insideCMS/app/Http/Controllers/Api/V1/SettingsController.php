9bfa0b84424ae2f18ced6b9cebb98a9dda5355e4
Сергей Субботин <zarodiny@yandex.ru>
2025-11-06 15:26:25 +1000

    feat: обновляет SettingsController

--- insideCMS/app/Http/Controllers/Api/V1/SettingsController.php
+++ insideCMS/app/Http/Controllers/Api/V1/SettingsController.php
@@ -8,43 +8,36 @@ use App\Models\Site_settings;
 
 class SettingsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-        return Site_settings::all();
+        $settings = Site_settings::all();
+        try {
+            if($settings!== null) {
+                return response()->json([
+                    'status' => 'success',
+                    'data' => $settings,
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 'error',
+                    'errors' => ['not found'],
+                ], 404);
+            }
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'errors' => [$e->getMessage()],
+            ], 500);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(string $slug)
     {
-
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        $settings = Site_settings::where('slug', $slug)->firstOrFail();
+        return response()->json([
+            'status' => 'success',
+            'data' => $settings,
+        ], 200);
     }
 }
