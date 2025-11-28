e053f6ca7adb8b9f79409a7004bc694927b1413d
Сергей Субботин <zarodiny@yandex.ru>
2025-11-28 16:44:12 +1000

    chore: добавляет импорт класса Validator в контроллер FileController для поддержки валидации загружаемых файлов

--- insideCMS/app/Http/Controllers/FileController.php
+++ insideCMS/app/Http/Controllers/FileController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\FileModel;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class FileController extends Controller
 {
